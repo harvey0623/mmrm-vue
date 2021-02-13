e3196b99bca25af2d3830082a1233ead76712042
harvey <harvey@henghung.com>
2021-02-13 11:05:40 +0800

    忘記密碼改用composition-api

--- src/views/member/forgotPassword.vue
+++ src/views/member/forgotPassword.vue
@@ -1,7 +1,9 @@
 <template src="./html/forgotPassword.html"></template>
 
 <script>
+import { ref, reactive } from '@vue/composition-api';
 import { memberApi } from '@/api/member.js';
+import { registerList } from '@/composition-api/registerList.js';
 export default {
    name: 'forgotPassword',
    metaInfo() {
@@ -9,51 +11,47 @@ export default {
          title: this.$i18n.t('page.forgotPassword.title'),
       }
    },
-   data: () => ({
-      user: {
+   setup(props, { root }) {
+      let { questionList } = registerList();
+      let user = reactive({
          mobile: '0986104667',
          security_question: 'sq01',
          security_answer: 'aaa'
-      },
-      questionList: [
-         { "title": "您畢業的國小", "value": "sq01" },
-         { "title": "您最愛的寵物名字", "value": "sq02" },
-         { "title": "您最愛的食物", "value": "sq03" },
-         { "title": "您最好朋友的名字", "value": "sq04" },
-         { "title": "您最愛的電影", "value": "sq05" }
-      ],
-      msgOption: {
+      });
+      let msgOption = reactive({
          isOpen: false,
          message: '',
          eventName: 'forgotFeedBack',
-      },
-      isLoading: false,
-      forgotSuccess: false
-   }),
-   methods: {
-      async submitHandler() {
-         let isValid = await this.$refs.form.validate().then(res => res);
+      });
+      let isLoading = ref(false);
+      let forgotSuccess = ref(false);
+      let form = ref(null);
+
+      let submitHandler = async() => {
+         let isValid = await form.value.validate().then(res => res);
          if (!isValid) return;
-         this.isLoading = true;
-         let { status, token } = await this.verifyHandler();
-         if (status) this.$store.commit('auth/setPasswordToken', token);
-         this.isLoading = false;
-      },
-      async verifyHandler() {
-         let { status, info } = await memberApi.forget_password(this.user);
-         this.forgotSuccess = status;
-         this.msgOption.message = status ? '填寫成功' : info.rcrm.RM;
-         this.msgOption.isOpen = true;
-         return { status, token: info.results.temp_access_token };
-      },
-      forgotFeedBack() {
-         if (this.forgotSuccess) this.$router.push('/member/verifyPassword')
-         this.msgOption.isOpen = false;
+         isLoading.value = true;
+         let { status, token } = await verifyHandler();
+         if (status) root.$store.commit('auth/setPasswordToken', token);
+         isLoading.value = false;
       }
+
+      let verifyHandler = async() => {
+         let { status, info } = await memberApi.forget_password(user);
+         forgotSuccess.value = status;
+         msgOption.message = status ? '填寫成功' : info.rcrm.RM;
+         msgOption.isOpen = true;
+         return { status, token: info.results.temp_access_token };
+      }
+      
+      let forgotFeedBack = () => {
+         if (forgotSuccess.value) root.$router.push('/member/verifyPassword')
+         msgOption.isOpen = false;
+      }
+
+      return { questionList, user, isLoading, msgOption, form, submitHandler, verifyHandler, forgotFeedBack };
    }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
