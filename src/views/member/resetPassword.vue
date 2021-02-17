d01b3a7903f8ef8140ca60a6ca4043e834b42d6c
harvey <harvey@henghung.com>
2021-02-17 11:05:28 +0800

    重設密碼頁面的顯示密碼功能改用composition-api

--- src/views/member/resetPassword.vue
+++ src/views/member/resetPassword.vue
@@ -2,6 +2,8 @@
 
 <script>
 import { memberApi } from '@/api/member.js';
+import { ref, reactive, computed, onMounted } from '@vue/composition-api';
+import { showPassword } from '@/composition-api/showPassword.js';
 export default {
    name: 'restePassword',
    metaInfo() {
@@ -9,82 +11,60 @@ export default {
          title: this.$i18n.t('page.resetPassword.title'),
       }
    },
-   data: () => ({
-      visible1: false,
-      visible2: false,
-      pwInput: null,
-      confirmInput: null,
-      user: {
+   setup(props, context) {
+      let { isReady, visibleInfo, seeHandler } = showPassword(context);
+      let { root } = context;
+      let pwInput = ref(null);
+      let confirmInput = ref(null);
+      let form = ref(null);
+      let isLoading = ref(false);
+      let resetSuccess = ref(false);
+      let user = reactive({
          new_password: 'abc123',
          confrimPw: 'abc123'
-      },
-      msgOption: {
+      });
+      let msgOption = reactive({
          isOpen: false,
          message: '',
          eventName: 'resetFeedBack',
-      },
-      isLoading: false,
-      resetSuccess: false
-   }),
-   computed: {
-      passwordToken() {
-         return this.$store.state.auth.passwordToken;
-      },
-      hasPasswordToken() {
-         return this.passwordToken !== '';
-      }
-   },
-   methods: {
-      seeHandler(key) {
-         this[key] = !this[key];
-      },
-      setInputType({ status, el }) {
-         el.type = status ? 'text' : 'password';
-      },
-      async submitHandler() {
-         let isValid = await this.$refs.form.validate().then(res => res);
+      });
+      let passwordToken = computed(() => {
+         return root.$store.state.auth.passwordToken;
+      });
+      let hasPasswordToken = computed(() => {
+         return passwordToken.value !== '';
+      });
+      let submitHandler = async() => {
+         let isValid = await form.value.validate();
          if (!isValid) return;
-         this.isLoading = true;
-         await this.resetHandler();
-         this.isLoading = false;
-      },
-      async resetHandler() {
+         isLoading.value = true;
+         await resetHandler();
+         isLoading.value = false;
+      }
+      let resetHandler = async() => {
          let { status, info } = await memberApi.reset_password({
-            temp_access_token: this.$store.state.auth.passwordToken,
-            new_password: this.user.new_password
+            temp_access_token: passwordToken.value,
+            new_password: user.new_password
          });
-         this.resetSuccess = status;
-         this.msgOption.isOpen = true;
-         this.msgOption.message = status ? '修改密碼成功' : info.rcrm.RM;
-      },
-      resetFeedBack() {
-         if (this.resetSuccess) {
-            this.$store.commit('auth/setPasswordToken', '');
-            this.$router.replace('/login');
+         resetSuccess.value = status;
+         msgOption.isOpen = true;
+         msgOption.message = status ? '修改密碼成功' : info.rcrm.RM;
+      }
+      let resetFeedBack = () => {
+         if (resetSuccess.value) {
+            root.$store.commit('auth/setPasswordToken', '');
+            root.$router.replace('/login');
          }
-         this.msgOption.isOpen = false;
+         msgOption.isOpen = false;
       }
-   },
-   created() {
-      if (!this.hasPasswordToken) this.$router.push('/member/forgotPassword');
-   },
-   mounted() {
-      this.pwInput = this.$refs.pwInput;
-      this.confirmInput = this.$refs.confirmInput;
-      this.setInputType({ status: this.visible1, el: this.pwInput });
-      this.setInputType({ status: this.visible2, el: this.confirmInput });
-   },
-   watch: {
-      visible1(to) {
-         this.setInputType({ status: to, el: this.pwInput });
-      },
-      visible2(to) {
-         this.setInputType({ status: to, el: this.confirmInput });
-      }
+
+      onMounted(() => {
+         if (!hasPasswordToken.value) root.$router.push('/member/forgotPassword');
+      });
+
+      return { isReady, visibleInfo, seeHandler, pwInput, confirmInput, form, isLoading, resetSuccess, user, msgOption, submitHandler, resetFeedBack };
    }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
