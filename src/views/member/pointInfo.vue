31247b49cc6bc310c80dcda0440f7e440d2a754d
harvey <harvey@henghung.com>
2021-02-24 10:23:07 +0800

    過期點數未串接完

--- src/views/member/pointInfo.vue
+++ src/views/member/pointInfo.vue
@@ -3,7 +3,7 @@
 <script>
 import { ref, reactive, onMounted, computed, watch } from '@vue/composition-api';
 import { pointApi } from '@/api/point.js';
-import { memberApi } from '@/api/point.js';
+import { memberApi } from '@/api/member.js';
 import DateSidebar from '@/components/Sidebar/Date.vue';
 import dayjs from 'dayjs';
 import _ from 'lodash';
@@ -17,16 +17,9 @@ export default {
    setup(props, { root }) {
       let pointId = ref(0);
       let isLoading = ref(false);
+      let currentPointAmount = ref('');
       let userPoint = reactive({ data: {} });
-
-      let getTargetPointInfo = async() => {
-         let { info } = await pointApi.pointInfo({
-            point_id: [pointId.value],
-            full_info: false
-         });
-         userPoint.data = info.results.point_information[0];
-         console.log(userPoint.data);
-      }
+      let expiredPoint = reactive({ data: [] });
 
       let hasUserPoint = computed(() => {
          return !(_.isEmpty(userPoint.data));
@@ -36,20 +29,66 @@ export default {
          return hasUserPoint.value ? userPoint.data.title : '';
       });
 
-      let pointUsageTime = computed(() => { //點數始用時間
+      let pointUsageTime = computed(() => { //點數使用時間
          if (!hasUserPoint.value) return '';
          let startTime = splitDateTime(userPoint.data.point_circulate_start_datetime);
          let endTime = splitDateTime(userPoint.data.point_circulate_end_datetime);
          return `${startTime} ~ ${endTime}`;
       });
 
-       let hideDuartion = computed(() => { //隱藏時間
+      let hideDuration = computed(() => { //隱藏時間
          if (!hasUserPoint) return false;
          return userPoint.data.hide_duration;
       });
 
+      let expiredTotal = computed(() => { //即將到期筆數
+         return expiredPoint.data.length;
+      });
+
+      let hasExpiredPoint = computed(() => { //是否有過期點數
+         return expiredTotal.value > 0;
+      });
+
+      let expiredPointAmount = computed(() => { //到期點數加總
+         return expiredPoint.data.reduce((prev, current) => {
+            let number = cammaToNumber(current.amount);
+            prev += number;
+            return prev;
+         }, 0);
+      });
+
       let splitDateTime = (text) => text.split(' ')[0];
 
+      let cammaToNumber = (text) => {
+         let result = text.replace(/,/g, '');
+         return parseInt(result);
+      }
+
+      let getTargetPointInfo = async() => {
+         let { info } = await pointApi.pointInfo({
+            point_id: [pointId.value],
+            full_info: false
+         });
+         userPoint.data = info.results.point_information[0];
+      }
+
+      let getMemberPoint = async() => {
+         let { info } = await memberApi.member_summary();
+         let obj = info.results.point_summary.current_point.find(item => {
+            return item.point_id === pointId.value;
+         });
+         if (obj !== undefined) currentPointAmount.value = obj.amount;
+         else currentPointAmount.value = 0;
+      }
+
+      let getExpiredPoint = async() => {
+         let { info } = await pointApi.pointDueToExpire({ point_id: pointId.value });
+         // expiredPoint.data = info.results.point_due_to_expire;
+         expiredPoint.data = [
+            { "datetime": "2019/02/10 23:59:59", "amount": "1,000" },
+         ]
+      }
+      
       watch(() => root.$route, (val, oldVal) => {
          console.log(val);
       });
@@ -58,10 +97,12 @@ export default {
          isLoading.value = true;
          pointId.value = parseInt(root.$route.params.point_id);
          await getTargetPointInfo();
+         await getMemberPoint();
+         await getExpiredPoint();
          isLoading.value = false;
       });
 
-      return { isLoading, pointName, pointUsageTime, hideDuartion };
+      return { isLoading, pointName, pointUsageTime, hideDuration, hasExpiredPoint, expiredTotal, expiredPointAmount, currentPointAmount, hasUserPoint };
    },
    components: {
       DateSidebar
