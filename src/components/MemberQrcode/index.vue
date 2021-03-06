f0327b5e9552dbcc5813f8fcbd16348a44050627
harvey <harvey@henghung.com>
2021-03-06 12:14:21 +0800

    完成會員qrcode顯示

---
+++ src/components/MemberQrcode/index.vue
@@ -0,0 +1,64 @@
+<template>
+   <img ref="qrcodeImg">
+</template>
+
+<script>
+import { ref, computed, watch, toRefs } from '@vue/composition-api';
+import _ from 'lodash';
+import Qrious from 'qrious';
+export default {
+   props: {
+      qrcodeInfo: {
+         type: Object,
+         required: true
+      }
+   },
+   setup(props, context) {
+      let qrcodeImg = ref(null);
+
+      let hasData = computed(() => {
+         return !(_.isEmpty(props.qrcodeInfo));
+      });
+
+      let createQrcodeSchema = (cardData, vehicleCode) => { //產生qrcode資料結構
+         let total = cardData.length;
+         let endIndex = vehicleCode !== undefined ? total -1 : total;
+         let schema = {
+            source: {
+               system: 'MMRM',
+               app: '{{MMRM}}',
+               type: 'member'
+            },
+            card_info: cardData.slice(0, endIndex),
+            invoice_info: [
+               {
+                  key: 'einvoice_carrier_no',
+                  value: vehicleCode !== undefined ? vehicleCode : ''
+               }
+            ]
+         };
+         return JSON.stringify(schema);
+      }
+      
+      let createQrcode = (cardData, vehicleCod) => { //產生qrcode圖型
+         new Qrious({
+            element: qrcodeImg.value,
+            value: createQrcodeSchema(cardData, vehicleCod),
+            size: 220,
+         });
+      }
+
+      watch(() => hasData.value, (val) => {
+         if (!val) return;
+         let { normalCode, vehicleCode } = toRefs(props.qrcodeInfo);
+         createQrcode(normalCode.value, vehicleCode.value );
+      }, {
+         immediate: true
+      });
+
+      return { qrcodeImg };
+   }
+}
+</script>
+
+<style></style>
--- src/views/member/memberCard.vue
+++ src/views/member/memberCard.vue
@@ -4,7 +4,7 @@
 import { ref, reactive, onMounted, computed } from '@vue/composition-api';
 import { memberApi } from '@/api/member.js';
 import { levelApi } from '@/api/level.js';
-import Qrious from 'qrious';
+import MemberQrcode from '@/components/MemberQrcode/index.vue';
 export default {
    name: 'memberCard',
    metaInfo() {
@@ -18,7 +18,7 @@ export default {
       let memberLevel = ref('');
       let barcodeTypeId = ref(1);
       let barcodeMapping = { 1: '切換一維條碼', 2: '切換二維條碼' };
-      let qrcodeEl = ref(null);
+      let qrcodeInfo = reactive({ data: {} });
 
       let barcodeTypeText = computed(() => {
          return barcodeMapping[barcodeTypeId.value];
@@ -26,34 +26,6 @@ export default {
 
       let switchBarcodeType = () => { //切換條碼類型
          barcodeTypeId.value = barcodeTypeId.value === 1 ? 2 : 1;
-      }
-
-      let createQrcodeSchema = (cardData, vehicleCode) => { //產生qrcode資料結構
-         let total = cardData.length;
-         let endIndex = vehicleCode !== undefined ? total -1 : total;
-         let schema = {
-            source: {
-               system: 'MMRM',
-               app: '{{MMRM}}',
-               type: 'member'
-            },
-            card_info: cardData.splice(0, endIndex),
-            invoice_info: [
-               {
-                  key: 'einvoice_carrier_no',
-                  value: vehicleCode !== undefined ? vehicleCode : ''
-               }
-            ]
-         };
-         return JSON.stringify(schema);
-      }
-      
-      let createQrcode = (cardData, vehicleCod) => { //產生qrcode圖型
-         new Qrious({
-            element: qrcodeEl.value,
-            value: createQrcodeSchema(cardData, vehicleCod),
-            size: 220,
-         });
       }
 
       onMounted(async () => {
@@ -64,19 +36,22 @@ export default {
          let levelId = memberSummary.info.results.level_summary.current_level.level_id;
          let levelInfo = await levelApi.info({ level_id: [levelId] });
          let memberCard = await memberApi.member_card().then(res => {
-            return res.info.results.member_card.code_info.card_info[0];
+            return res.info.results.member_card.code_info.card_info;
          });
          let vehicleCode = memberProfile.info.results.member_profile.einvoice_carrier_no;
          memberName.value = memberProfile.info.results.member_profile.name;
          memberLevel.value = levelInfo.info.results.level_information[0].title;
          cardSource = cardSource.concat(memberCard);
          if (vehicleCode !== undefined) cardSource.push({ key: '載具條碼', value: vehicleCode });
-         createQrcode(cardSource, vehicleCode);
+         qrcodeInfo.data = { normalCode: cardSource, vehicleCode };
 
          isLoading.value = false;
       });
 
-      return { isLoading, memberName, memberLevel, switchBarcodeType, barcodeTypeText, qrcodeEl, barcodeTypeId };
+      return { isLoading, memberName, memberLevel, switchBarcodeType, barcodeTypeText, barcodeTypeId, qrcodeInfo };
+   },
+   components: {
+      MemberQrcode
    }
 }
 </script>
